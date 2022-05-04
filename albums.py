07f583b4d690d0709ea1939eff26b8a20aa12a05
Skylar Lynner <skylar.lynner@gmail.com>
2022-05-03 22:45:53 -0500

    refactor file opening and closing

--- albums.py
+++ albums.py
@@ -1,17 +1,21 @@
-ALBUMS_FILE = "../trackers_db/album.txt"
+#!/usr/bin/env python3
+ALBUMS_FILE = "album.txt"
 
 def all_albums():
-	lines = open(ALBUMS_FILE).read().splitlines()
+	album_file = open(ALBUMS_FILE)
+	lines = album_file.read().splitlines()
 	for album in lines:
 		album, artists, languages = album.split("|")
 		artists = artists.split(";")
 		languages = languages.split(";")
 		print(f"Album: {album}\tArtist: {artists}\tLanguage: {languages}")
+	album_file.close()
 
 def add_album():
 	print("Separate multiple artists and languages with a semicolon and no spaces.")
 	album = input("Album title: ")
 	artists = input("Artists: ")
 	languages = input("Language: ")
-	open(ALBUMS_FILE, "a").write(album + "|" + artists + "|" + languages + "\n")
-
+	album_file = open(ALBUMS_FILE, "a")
+	album_file.write(album + "|" + artists + "|" + languages + "\n")
+	album_file.close()
--- quotes.py
+++ quotes.py
@@ -1,16 +1,21 @@
+#!/usr/bin/env python3
 import random
 
 QUOTES_FILE = "quotes.txt"
 
 def add_quote():
-	quote = input('Quote: ')
-	citation = input('Citation: ')
+    quote = input('Quote: ')
+    citation = input('Citation: ')
 
-	print(quote + " -" + citation)
-	open(QUOTES_FILE, "a").write(quote + "|" + citation + "\n")
+    print(quote + " -" + citation)
+    quote_file = open(QUOTES_FILE, "a")
+    quote_file.write(quote + "|" + citation + "\n")
+    quote_file.close()
 
 def random_quote():
-    lines = open(QUOTES_FILE).read().splitlines()
+    quote_file = open(QUOTES_FILE)
+    lines = quote_file.read().splitlines()
     quote = random.choice(lines)
     quote, attributed = quote.split("|")
     print(f"\n{quote}\n  - {attributed}\n")
+    quote_file.close()
