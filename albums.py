418926ed42ab1b5dd1fea71d418638e9ccd33d77
Skylar Lynner <skylar.lynner@gmail.com>
2022-05-15 21:22:51 -0500

    add album class and update print statement for all_albums method

--- albums.py
+++ albums.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 ALBUMS_FILE = "album.txt"
 
+class Album:
+	def __init__(self, title, artists, languages):
+		self.title = title
+		self.artists = artists
+		self.languages = languages
+
+	def __str__(self):
+		artist_string = ' '.join(str(artist) for artist in self.artists)
+		language_string = ' '.join(str(language) for language in self.languages)
+		return f"{self.title} by {artist_string} {language_string}"
+
 def all_albums():
 	album_file = open(ALBUMS_FILE)
 	lines = album_file.read().splitlines()
@@ -8,7 +19,7 @@ def all_albums():
 		album, artists, languages = album.split("|")
 		artists = artists.split(";")
 		languages = languages.split(";")
-		print(f"Album: {album}\tArtist: {artists}\tLanguage: {languages}")
+		print(Album(album, artists, languages))
 	album_file.close()
 
 def add_album():
